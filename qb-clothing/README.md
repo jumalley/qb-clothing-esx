be44314036fe879f278ac687d49cafb58a9c7b3e
Ju <50602425+jumalley@users.noreply.github.com>
2023-04-08 19:42:12 +0200

    Add files via upload

---
+++ qb-clothing/README.md
@@ -0,0 +1,9 @@
+# qb-clothing
+FiveM QBus-Clothing Script ESX Framework converted version.
+
+# Commands
+
+"skin", function(source, args)
+"mask", function(source, args)
+"glasses", function(source, args)
+"helm", function(source, args)
---
+++ qb-clothing/fxmanifest.lua
@@ -0,0 +1,27 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+lua54 'yes'
+
+author 'Ju'
+description 'QB Clothing ESX Converted'
+
+ui_page "html/index.html"
+
+server_scripts {
+	"config.lua",
+	"server/main.lua",
+}
+
+client_scripts {
+	"config.lua",
+	"client/main.lua",
+}
+
+files {
+	'html/index.html',
+	'html/style.css',
+	'html/reset.css',
+	'html/script.js',
+}
